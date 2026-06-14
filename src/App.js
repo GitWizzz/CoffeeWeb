b980e3536d67806e830ee86680e0cd6ce6e5da0e
Shivam Kumar <sk744826744@gmail.com>
2026-06-14 14:00:32 +0530

    Fixed Vercel deployment by bypassing CI warnings in build script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
